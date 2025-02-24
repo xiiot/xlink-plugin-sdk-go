f0825889484c6efcba9eca3b3cba55943076fb79
Wang Yongkang <yongkang.wang@outlook.com>
2025-02-24 15:25:31 +0800

    feat(grpc): 增加请求 ID 映射并更新 gRPC 方法
    - 在 RequestArgs 和 ResponseResult 结构中添加 request_id 字段
    - 更新 gRPC 方法以支持新的请求 ID 参数
    - 修复了缺少请求 ID 映射的问题

--- grpc_report.go
+++ grpc_report.go
@@ -13,22 +13,24 @@ type gRPCReportClient struct {
 
 func (m *gRPCReportClient) Post(req *Request) (*Response, error) {
 	res, err := m.client.Post(context.Background(), &proto.RequestArgs{
-		Request: req.Req,
+		Request:   req.Req,
+		RequestId: req.RequestID,
 	})
 	if err != nil {
 		return nil, err
 	}
-	return &Response{Data: res.Data}, nil
+	return &Response{Data: res.Data, RequestID: res.RequestId}, nil
 }
 
 func (m *gRPCReportClient) State(req *Request) (*Response, error) {
 	res, err := m.client.State(context.Background(), &proto.RequestArgs{
-		Request: req.Req,
+		Request:   req.Req,
+		RequestId: req.RequestID,
 	})
 	if err != nil {
 		return nil, err
 	}
-	return &Response{Data: res.Data}, nil
+	return &Response{Data: res.Data, RequestID: res.RequestId}, nil
 }
 
 type gRPCReportServer struct {
@@ -38,20 +40,22 @@ type gRPCReportServer struct {
 
 func (m *gRPCReportServer) Post(_ context.Context, req *proto.RequestArgs) (*proto.ResponseResult, error) {
 	res, err := m.Impl.Post(&Request{
-		Req: req.Request,
+		Req:       req.Request,
+		RequestID: req.RequestId,
 	})
 	if err != nil {
 		return &proto.ResponseResult{}, err
 	}
-	return &proto.ResponseResult{Data: res.Data}, nil
+	return &proto.ResponseResult{Data: res.Data, RequestId: res.RequestID}, nil
 }
 
 func (m *gRPCReportServer) State(_ context.Context, req *proto.RequestArgs) (resp *proto.ResponseResult, err error) {
 	res, err := m.Impl.State(&Request{
-		Req: req.Request,
+		Req:       req.Request,
+		RequestID: req.RequestId,
 	})
 	if err != nil {
 		return &proto.ResponseResult{}, err
 	}
-	return &proto.ResponseResult{Data: res.Data}, nil
+	return &proto.ResponseResult{Data: res.Data, RequestId: res.RequestID}, nil
 }
