1c8c27aa048a24e9586ad4713c3ea01ed5f478bc
Wang Yongkang <yongkang.wang@outlook.com>
2025-03-01 14:54:32 +0800

    feat(comctx): 新增 LazyValue 类型及其反序列化方法- 在 comctx 包中添加了 NewRequestID 函数，用于生成新的请求 ID - 新增 LazyValue 类型，用于延迟反序列化操作 - 实现了 LazyValue 的 Unmarshal 方法，支持从 JSON 数据或值进行反序列化

--- comctx/message.go
+++ comctx/message.go
@@ -1,6 +1,9 @@
 package comctx
 
-import "github.com/satori/go.uuid"
+import (
+	"encoding/json"
+	"github.com/satori/go.uuid"
+)
 
 type Message struct {
 	Kind     MessageKind       `yaml:"kind" json:"kind"`
@@ -15,11 +18,25 @@ const (
 	MessageDeviceState  MessageKind = "deviceState"
 )
 
+func NewRequestID() string {
+	return uuid.NewV4().String()
+}
+
 type LazyValue struct {
 	Value interface{}
 	doc   []byte
 }
 
-func NewRequestID() string {
-	return uuid.NewV4().String()
+func (v *LazyValue) Unmarshal(obj interface{}) error {
+	if v.doc != nil {
+		return json.Unmarshal(v.doc, obj)
+	}
+	if v.Value != nil {
+		bs, err := json.Marshal(v.Value)
+		if err != nil {
+			return err
+		}
+		return json.Unmarshal(bs, obj)
+	}
+	return nil
 }
